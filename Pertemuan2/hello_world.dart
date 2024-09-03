ff815f7a3288c642aea4cd9f2989e6bfc7d21137
Afifah Rahma <afifahhrahmaa03@gmail.com>
2024-09-03 14:30:03 +0700

    first commit

---
+++ Pertemuan2/hello_world.dart
@@ -0,0 +1,13 @@
+
+// void main() {
+//   for (int i = 0; i < 5; i++) {
+//     print('hello ${i + 1}');
+//   }
+// }
+
+
+void main() {
+  for (int i = 0; i < 10; i++) {
+    print('Saya adalah Afifah, sekarang berumur ${18 - i}');
+  }
+}
